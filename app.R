18398e89dd1f1270c1c6e0d7f45f31245c57e493
Kai Lewis <kailewis104@gmail.com>
2021-10-04 20:35:59 +1300

    Testing dev environment

--- app.R
+++ app.R
@@ -19,7 +19,7 @@ ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(
             sliderInput("bins",
-                        "Test Number of bins:",
+                        "Number of bins:",
                         min = 1,
                         max = 50,
                         value = 30)
